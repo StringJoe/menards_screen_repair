3886f39c83c4b40ce3a3fe889caa6f5c9830fe3e
joe <santayjoseph@gmail.com>
2022-07-19 23:05:41 -0500

    completed java code for frame_measurements. also created smaller xml file so more phones can use the application

--- app/src/main/java/com/joe/menardsscreenrepair/frameMeasurements.java
+++ app/src/main/java/com/joe/menardsscreenrepair/frameMeasurements.java
@@ -10,17 +10,23 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+import java.text.Format;
+
 public class frameMeasurements extends AppCompatActivity {
 
     EditText screenWidth, screenLength;
-    Button bigCornerBtn, smallCornerBtn, calcBtn, resetBtn, menuBtn;
+    Button bigCornerBtn, smallCornerBtn, calcBtn, resetBtn, menuBtn, lengthCountFraction, widthCountFraction;
     TextView totalWidth, totalLength;
+
+    double lengthCount, widthCount;
 
     double thirteenOver = 1.625;
     double oneOverEight = 1.5;
-    double width, length;
+    double width, length, widthFraction, lengthFraction;
     double cornerToSubtract;
 
+    Format myFormatter = new DecimalFormat("##");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +34,8 @@ public class frameMeasurements extends AppCompatActivity {
 
         screenWidth = findViewById(R.id.screenWidth);
         screenLength = findViewById(R.id.screenLength);
+        lengthCountFraction = findViewById(R.id.lengthFractionCount);
+        widthCountFraction = findViewById(R.id.widthFractionCount);
 
         bigCornerBtn = findViewById(R.id.bigCornerBtn);
         smallCornerBtn = findViewById(R.id.smallCornerBtn);
@@ -38,20 +46,53 @@ public class frameMeasurements extends AppCompatActivity {
         totalWidth = findViewById(R.id.totalWidth);
         totalLength = findViewById(R.id.totalLength);
 
+        lengthCountFraction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(lengthCount <= 14)
+                {
+                    lengthCount += 1;
+                }
+
+                lengthCountFraction.setText(myFormatter.format(lengthCount)+"/16\"");
+            }
+        });
+
+        widthCountFraction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(widthCount <= 14)
+                {
+                    widthCount += 1;
+                }
+
+                widthCountFraction.setText(myFormatter.format(widthCount)+"/16\"");
+            }
+        });
+
         resetBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 width = 0;
                 length = 0;
+
                 screenWidth.setText("");
                 screenLength.setText("");
                 screenWidth.setHint("Enter Screen Width");
                 screenLength.setHint("Enter Screen Length");
+
                 totalWidth.setText("Cut width to: ");
                 totalLength.setText("Cut Length to: ");
+
                 cornerToSubtract = 0;
                 smallCornerBtn.setTextColor(Color.WHITE);
                 bigCornerBtn.setTextColor(Color.WHITE);
+
+                lengthCountFraction.setText("0/16");
+                widthCountFraction.setText("0/16");
+
+                lengthCount = 0;
+                widthCount = 0;
             }
         });
 
@@ -60,9 +101,19 @@ public class frameMeasurements extends AppCompatActivity {
             public void onClick(View v) {
                 width = Double.parseDouble(screenWidth.getText().toString());
                 length = Double.parseDouble(screenLength.getText().toString());
+                /*
+                width = width - cornerToSubtract;
 
-                totalWidth.setText("Cut width to: "+(width-cornerToSubtract));
-                totalLength.setText("Cut Length to: "+(length-cornerToSubtract));
+                widthFraction = width - (long) width;
+
+                width = width - widthFraction;
+                widthFraction = widthFraction / 0.0625;
+                widthFraction = (int)widthFraction;*/
+
+                calculateLengthWidth();
+
+                totalWidth.setText("Cut width to: " + myFormatter.format(width) + " " + myFormatter.format(widthFraction) + "/16\"");
+                totalLength.setText("Cut Length to: " + myFormatter.format(length) + " " + myFormatter.format(lengthFraction) + "/16\"");
             }
         });
 
@@ -96,5 +147,57 @@ public class frameMeasurements extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    public void calculateLengthWidth()
+    {
+        // gather data from the editText components
+        width = Double.parseDouble(screenWidth.getText().toString());
+        length = Double.parseDouble(screenLength.getText().toString());
+
+        addFractionToWidthLength();
+
+        // subtract the corner pieces
+        width = width - cornerToSubtract;
+        length = length - cornerToSubtract;
+
+        // separate decimal values from width and length
+        widthFraction = width - (long) width;
+        lengthFraction = length - (long) length;
+
+        // convert width and length back to full number
+        width = width - widthFraction;
+        length = length - lengthFraction;
+
+        // divide the decimals by 0.0625 to get the numerator
+        if(widthFraction > 0)
+        {
+            widthFraction = widthFraction / 0.0625;
+        }
+        else
+        {
+            widthFraction = 0;
+        }
+
+        if(lengthFraction > 0)
+        {
+            lengthFraction = lengthFraction / 0.0625;
+        }
+        else
+        {
+            lengthFraction = 0;
+        }
+
+    }
+
+    public void addFractionToWidthLength()
+    {
+        // convert the button data to decimal form
+        lengthCount = lengthCount / 16;
+        widthCount = widthCount / 16;
+
+        // add the decimals to the width and length values
+        width = width + widthCount;
+        length = length + lengthCount;
     }
 }
