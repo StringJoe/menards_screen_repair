41c50504fbc996c02b154a6467e6c0fc98f5f896
joe <santayjoseph@gmail.com>
2022-07-25 23:25:06 -0500

    fixed application crash when width or length not entered in screen size. also fixed bug where extra button presses kept adding money to final total

--- app/src/main/java/com/joe/menardsscreenrepair/screenSize.java
+++ app/src/main/java/com/joe/menardsscreenrepair/screenSize.java
@@ -1,13 +1,16 @@
 package com.joe.menardsscreenrepair;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class screenSize extends AppCompatActivity {
 
@@ -61,7 +64,9 @@ public class screenSize extends AppCompatActivity {
         equalsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 calculateScreenFootage();
+
             }
         });
 
@@ -76,8 +81,26 @@ public class screenSize extends AppCompatActivity {
     public void calculateScreenFootage()
     {
         // get width and length from editText components
-        width = Integer.parseInt(screenWidth.getText().toString());
-        length = Integer.parseInt(screenLength.getText().toString());
+        if(screenWidth.getText().toString().equals("") && screenLength.getText().toString().equals(""))
+        {
+            Toast.makeText(this, "You need to enter a width and length", Toast.LENGTH_SHORT).show();
+        }
+        else if(screenWidth.getText().toString().equals(""))
+        {
+            Toast.makeText(this, "You need to enter a width", Toast.LENGTH_SHORT).show();
+        }
+        else if(screenLength.getText().toString().equals(""))
+        {
+            Toast.makeText(this, "You need to enter a length", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            width = Integer.parseInt(screenWidth.getText().toString());
+            length = Integer.parseInt(screenLength.getText().toString());
+            equalsBtn.setEnabled(false);
+            equalsBtn.setBackgroundColor(Color.GRAY);
+        }
+
 
         //calculate perimeter for spline values
         perimeter = (width*2) + (length*2);
@@ -135,10 +158,12 @@ public class screenSize extends AppCompatActivity {
         screenWidth.setHint("Enter Width");
         screenLength.setHint("Enter Length");
 
-        squareFt.setText("Sq ft: ");
+        squareFt.setText("Sq ft: 0");
 
         width = 0;
         length = 0;
         squareFootage = 0;
+        equalsBtn.setEnabled(true);
+        equalsBtn.setBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.menards_green, null));
     }
 }
